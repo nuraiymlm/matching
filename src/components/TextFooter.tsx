bf237f7fd76c1c8724a744ca1d9fdc9e577e3005
Nuraiym Mukhambetova <148516119+nuraiymlm@users.noreply.github.com>
2026-06-26 21:39:43 +0500

    Update TextFooter.tsx

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -20,7 +20,7 @@ export default function TextFooter() {
       <h1
         className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
-        to reveal <br /> <span className="text-gray-400">the surprise</span>
+       just enjoy <br /> <span className="text-gray-400">the game </span>
       </h1>
 
       {/* Copyright */}
